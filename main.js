711aec7d5cdc770c5bc8cfb3bc49b3dc3248ad6d
Kamchybek95 <13.kamchybek1995@gmail.com>
2023-02-25 19:31:47 +0300

    V1.0

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        loop: true,
+    });
+
+    
+});
+
+
+
+Fancybox.bind("#gallery a", {
+    groupAll: true,
+});
+
+const burger = document.getElementById('burger'); 
+const show = document.getElementById('show-burger'); 
+let bool = true; 
+burger.addEventListener("click", (btn) => { 
+    if (bool) { 
+        show.classList.add('active'); 
+        bool = !bool 
+    } else { 
+        show.classList.remove('active'); 
+        bool = !bool; 
+    } 
+ 
+})
